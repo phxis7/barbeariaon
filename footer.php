f457f436a48df10ed68fc56e2f4b1aea40eca4b5
phxis7 <paulo.benevides0990@gmail.com>
2025-06-18 21:28:45 -0300

    site on

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+  <div class="container text-center">
+    <p class="mb-0">&copy; <?php echo date("Y"); ?> Barbearia de Respeito. Todos os direitos reservados.</p>
+  </div>
+</footer>
